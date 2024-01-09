eb37279bb5611ac0014be6d66c8a7b908d16235d
cheeriosmilky <111781222+cheeriosmilky@users.noreply.github.com>
2024-01-09 11:34:23 -0500

    pt 2

--- NewsApp/Pages/NewsPage.xaml.cs
+++ NewsApp/Pages/NewsPage.xaml.cs
@@ -1,9 +1,26 @@
+using NewsApp.Models;
+using NewsApp.Services;
+
 namespace NewsApp.Pages;
 
 public partial class NewsPage : ContentPage
 {
+    public List<Article> ArticlesList { get; set; }
 	public NewsPage()
 	{
 		InitializeComponent();
+        ArticlesList = new List<Article>();
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        ApiServices apiService = new ApiServices();
+        var newsResult = await apiService.GetNews();
+        foreach ( var item in newsResult.Articles )
+        {
+            ArticlesList.Add(item);
+        }
+        CvNews.ItemsSource = ArticlesList;
+    }
 }
---
+++ NewsApp/Services/ApiServices.cs
@@ -0,0 +1,23 @@
+﻿using NewsApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Services
+{
+    public class ApiServices
+    {
+
+        public async Task<Root> GetNews()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=4afede633af5ee44b4ea04f7ae938361&topic=breaking-news");
+
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+
+    }
+}
